afc25e0c6c30f20de06ebc31aa4bbb7002aa6495
Adrien Menella <adrien.menella@gmail.com>
2016-11-17 16:25:19 +0200

    load classification and regression files into main file

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
+
+import regression as rg
+import classification as cl
 import os
 
 dir = os.path.dirname(__file__)
 
 def load_file():
-    1+1
+    return "Load file"
 
 
 if __name__ == '__main__':
 
-    load_file()
+    print load_file()
 
+    print rg.test_regression()
+
+    print cl.test_classification()
