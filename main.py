7a0a4007937b7823be5bee1706ed8fe4d125561c
Adrien Menella <adrien.menella@gmail.com>
2016-11-17 16:18:50 +0200

    add main file

---
+++ main.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+# Importing modules:
+
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+dir = os.path.dirname(__file__)
+
+def load_file():
+    1+1
+
+
+if __name__ == '__main__':
+
+    load_file()
+
