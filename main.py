c724be0699227364f3b04e05a3bc3e8f7e54e6f8
thovex38 <germain.geoffroy@gmail.com>
2016-11-18 16:37:03 +0200

    multinomial_implemented

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import numpy as np
 import regression as rg
 import classification as cl
 import os
+from sklearn.naive_bayes import MultinomialNB
 
 dir = os.path.dirname(__file__)
 
@@ -18,9 +19,21 @@ def load_file(c):
 
 
 if __name__ == '__main__':
-    filename = os.path.join(dir,'data/classification_dataset_training.csv')
-    matrice=load_file(filename)#matrice with 5000 rows and 52 columns
-    print(matrice[0][51])
-    print rg.test_regression()
+    training_file = os.path.join(dir,'data/classification_dataset_training.csv')
+    test_file = os.path.join(dir,'data/classification_dataset_testing.csv')
+    solution_file = os.path.join(dir,'data/classification_dataset_testing_solution.csv')
+    training=load_file(training_file)#matrice with 5000 rows and 52 columns
+    test2D=load_file(test_file)
+    solution=load_file(solution_file)
 
-    print cl.test_classification()
+    likelihood0, likelihood1, prior0, prior1=cl.apprentissage_multinomiale(training)
+
+
+    prediction=cl.test(likelihood0,likelihood1,prior0,prior1,test2D)
+    sol=cl.extract(solution)
+    pourcentage=0.
+    for i in range(0,1000):
+       if(sol[i]==prediction[i]):
+            pourcentage+=1
+    pourcentage=float(pourcentage)/1000.
+    print(pourcentage)
